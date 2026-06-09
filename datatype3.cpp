3e031f0bf341cf33076285bdfb7f36e5a11fa330
anshikasisodiya4 <anshikasisodiya81@gmail.com>
2026-06-09 21:05:10 +0530

    commit

---
+++ datatype3.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+int global=6;
+
+void sum(){
+    int a;
+    cout<<global;// global first then local variable
+}
+// if both global and local variable have same name then preference will be given to local variable
+
+int main(){
+    int global=12;
+    global=14;
+
+    int a = 2 ,b = 3;
+    int c = 4;
+    float pi = 3.14;
+    char d='u';
+    bool is_true=false;
+    sum();
+
+    // cout<<"The value of a is "<<a<<".\nThe value of b is "<<b;
+    // cout<<". \nThe value of c is "<<c;
+    // cout<<".\nThe value of d id "<<d;
+    // cout<<global;
+    cout<<is_true;// 1 in optput represents true
+
+    return 0;
+}
