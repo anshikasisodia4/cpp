a5beb416a8ebf3f973f4d76eee60fd6058d12056
anshikasisodiya4 <anshikasisodiya81@gmail.com>
2026-06-28 08:46:26 +0530

    commit

---
+++ fibonacco35.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int fibonacci(int n){
+    if(n<2){
+        return 1;
+    }
+    return fibonacci(n-2) + fibonacci(n-1);
+}
+
+int main(){
+    int a;
+    cout<<"Enter a number :";
+    cin>>a;
+    cout<<"The term in Fibonacci Series at position "<<a<<" is : "<<fibonacci(a)<<endl;
+    return 0;
+}
