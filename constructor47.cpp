b2cb738d45910d804dde86233f89d579368b5564
anshikasisodiya4 <anshikasisodiya81@gmail.com>
2026-07-05 13:06:35 +0530

    commit

--- constructor47.cpp
+++ constructor47.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
 using namespace std;
 
+class Complex{
+    int a ,b;
+   
+    // creating a constructor
+    // constructor is a special member function with same name as of the class 
+    //it is used to initialize the objects of its class
+    // it is automatically invoked whenever ab object is created 
+    // it is used to initialise the objects of its class
+    public:
+     // constructor declaration 
+    Complex (void); 
+    void printNumber(){
+        cout<<"Your number is "<<a<<" + "<<b<<"i"<<endl;
+
+    }        
+};
+Complex:: Complex(void){
+    a = 10;
+    b=0;
+}
+
 int main(){
-    
+    Complex c;
+    c.printNumber();
     return 0;
 }
