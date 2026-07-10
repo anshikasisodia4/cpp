0c96a4008d85473d306eb72cd46d6fb2ab288cea
anshikasisodiya4 <anshikasisodiya81@gmail.com>
2026-07-10 22:53:58 +0530

    commit

--- 61Calculator.cpp
+++ 61Calculator.cpp
@@ -2,75 +2,79 @@
 #include <cmath>
 using namespace std;
 
-class SimpleCalculator{
-    protected:
-        float a ;
-        float b;
-        float sum,minus,division,product;
-        public:
-       
-        void get1(){
-            cout<<"Simple Calculator"<<endl;
-            cout<<"Enter two numbers: "<<endl;
-            cin>>a>>b;
-        }
-        void Calculate1(){
-            sum = a+b;
-            minus = a-b;
-            product = a*b;
-            division =a/b; 
+class SimpleCalculator
+{
+protected:
+    float a;
+    float b;
+    float sum, minus, division, product;
 
-        }
-        void show1(){
-            cout<<"The sum is "<<sum<<endl;
-            cout<<"The subtraction is "<<minus<<endl;
-            cout<<"The product is "<<product<<endl;
-            cout<<"The division is "<<division<<endl;
-        
-        };
-
-};
-class ScienificCalculator{
-    protected:
-      float a ;
-      float b;
-      float exponential, squareRoot, power,logarithm;
-    public:
-  
-    void get2(){
-        cout<<endl;
-        cout<<"Scientific Calculator"<<endl;
-        cout<<"Enter two numbers: "<<endl;
-        cin>>a>>b;
+public:
+    void get1()
+    {
+        cout << "Simple Calculator" << endl;
+        cout << "Enter two numbers: " << endl;
+        cin >> a >> b;
     }
-    void Calculate2(){
-        exponential = exp(a);
-        power = pow(a,b);
-        logarithm = (a>=0) ? log(a): NAN;
-        squareRoot = (a>=0)?sqrt(a):NAN;
+    void Calculate1()
+    {
+        sum = a + b;
+        minus = a - b;
+        product = a * b;
+        division = a / b;
     }
-    void show2(){
-        cout<<"The exponential of "<<a<<" is "<<exponential<<endl;
-        cout<<"The square Root of "<<a<<" is "<<squareRoot<<endl;
-        cout<<"The powerCalculation is "<<power<<endl;
-        cout<<"The log of "<<a<<" is " <<logarithm<<endl;
+    void show1()
+    {
+        cout << "The sum is " << sum << endl;
+        cout << "The subtraction is " << minus << endl;
+        cout << "The product is " << product << endl;
+        cout << "The division is " << division << endl;
     };
-
-};
-class HybridCalculator: public ScienificCalculator, public SimpleCalculator{
-
-   public:
-  
-   void show(){
-    cout<<"Hybrid Calculator"<<endl;
-    cout<<"Simple calculator sum +  scientific calculator : "<<sum+power<<endl;
-
-   }
-    
-
 };
 
-int main(){
+class ScienificCalculator
+{
+protected:
+    float a;
+    float b;
+    float exponential, squareRoot, power, logarithm;
+
+public:
+    void get2()
+    {
+        cout << endl;
+        cout << "Scientific Calculator" << endl;
+        cout << "Enter two numbers: " << endl;
+        cin >> a >> b;
+    }
+    void Calculate2()
+    {
+        exponential = exp(a);
+        power = pow(a, b);
+        logarithm = (a >= 0) ? log(a) : NAN;
+        squareRoot = (a >= 0) ? sqrt(a) : NAN;
+    }
+    void show2()
+    {
+        cout << "The exponential of " << a << " is " << exponential << endl;
+        cout << "The square Root of " << a << " is " << squareRoot << endl;
+        cout << "The powerCalculation is " << power << endl;
+        cout << "The log of " << a << " is " << logarithm << endl;
+    };
+};
+
+class HybridCalculator : public ScienificCalculator, public SimpleCalculator
+{
+public:
+    void show()
+    {
+        cout << "Hybrid Calculator" << endl;
+        cout << "Simple calculator sum +  scientific calculator : " << sum + power << endl;
+    }
+};
+
+int main()
+{
     HybridCalculator calc;
     calc.get1();
     calc.Calculate1();
