0ca10e227d3dc00c65281a2e580f466f1232bbfb
anshikasisodiya4 <anshikasisodiya81@gmail.com>
2026-07-12 18:59:39 +0530

    commit

--- 61Calculator.cpp
+++ 61Calculator.cpp
@@ -4,85 +4,72 @@ using namespace std;
 
 class SimpleCalculator
 {
-protected:
     float a;
     float b;
-    float sum, minus, division, product;
 
 public:
-    void get1()
+    void getData1()
     {
         cout << "Simple Calculator" << endl;
-        cout << "Enter two numbers: " << endl;
-        cin >> a >> b;
+        cout << "Enter first number: " << endl;
+        cin >> a;
+        cout << "Enter secod number :" << endl;
+        cin >> b;
     }
+
     void Calculate1()
     {
-        sum = a + b;
-        minus = a - b;
-        product = a * b;
-        division = a / b;
-    }
-    void show1()
-    {
-        cout << "The sum is " << sum << endl;
-        cout << "The subtraction is " << minus << endl;
-        cout << "The product is " << product << endl;
-        cout << "The division is " << division << endl;
+        cout << "The sum is " << a + b << endl;
+        cout << "The subtraction is " << a - b << endl;
+        cout << "The product is " << a * b << endl;
+        cout << "The division is " << a / b << endl;
     };
 };
 
 class ScienificCalculator
 {
-protected:
     float a;
     float b;
-    float exponential, squareRoot, power, logarithm;
 
 public:
-    void get2()
+    void getData2()
     {
         cout << endl;
         cout << "Scientific Calculator" << endl;
-        cout << "Enter two numbers: " << endl;
-        cin >> a >> b;
+        cout << "Enter first number " << endl;
+        cin >> a;
+        cout << "Enter second number :" << endl;
+        cin >> b;
     }
+
     void Calculate2()
     {
-        exponential = exp(a);
-        power = pow(a, b);
-        logarithm = (a >= 0) ? log(a) : NAN;
-        squareRoot = (a >= 0) ? sqrt(a) : NAN;
-    }
-    void show2()
-    {
-        cout << "The exponential of " << a << " is " << exponential << endl;
-        cout << "The square Root of " << a << " is " << squareRoot << endl;
-        cout << "The powerCalculation is " << power << endl;
-        cout << "The log of " << a << " is " << logarithm << endl;
+        cout << "The cos of " << a << " is " << cos(a) << endl;
+        cout << "The sin of " << a << " is " << sin(a) << endl;
+        cout << "The exponential is " << exp(a) << endl;
+        cout << "The tan of " << a << " is " << tan(a) << endl;
     };
 };
 
-class HybridCalculator : public ScienificCalculator, public SimpleCalculator
+// multiple inheritance using public mode
+
+class HybridCalculator : public SimpleCalculator, public ScienificCalculator
 {
 public:
     void show()
     {
         cout << "Hybrid Calculator" << endl;
-        cout << "Simple calculator sum +  scientific calculator : " << sum + power << endl;
+       
     }
 };
 
 int main()
 {
     HybridCalculator calc;
-    calc.get1();
-    calc.Calculate1();
-    calc.show1();
-    calc.get2();
+    calc.getData2();
     calc.Calculate2();
-    calc.show2();
-    calc.show();
+    calc.getData1();
+    calc.Calculate1();
 
     return 0;
 }
