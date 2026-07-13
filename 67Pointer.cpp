114b53162863074a5822497aaa29e6042efc6cc0
anshikasisodiya4 <anshikasisodiya81@gmail.com>
2026-07-13 18:44:03 +0530

    commit

--- 67Pointer.cpp
+++ 67Pointer.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    
+int main()
+{
+    int a = 4;
+    int* ptr = &a;
+    *(ptr) = 999;
+    cout << "The value of a is " << *(ptr) << endl;
+
+    // new operator
+    float *p = new float(40.78);
+    cout << "The value at address p is " << *(p) << endl;
+
+    int *arr = new int[3];
+    arr[0] = 10;
+    // arr[1] = 20;
+    *(arr + 1) = 20;
+    arr[2] = 30;
+
+    // delete operator
+
+    // delete[] arr;
+    cout << "The value at arr[0] is " << arr[0] << endl;
+    cout << "The value at arr[1] is " << arr[1] << endl;
+    cout << "The value at arr[2] is " << arr[2] << endl;
+
     return 0;
 }
