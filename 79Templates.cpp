367d38e22e0147fbae13974a595dd6abe7e69e8b
anshikasisodia4 <anshikasisodiya81@gmail.com>
2026-07-27 06:29:11 +0530

    Initial commit

--- 79Templates.cpp
+++ 79Templates.cpp
@@ -1,7 +1,35 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    
+/*
+template <class T1, class T2>
+class nameOfClass{
+body
+}
+
+*/
+template <class T1, class T2>
+class Myclass
+{
+public:
+    T1 data1;
+    T2 data2;
+    Myclass(T1 a, T2 b)
+    {
+        data1 = a;
+        data2 = b;
+    }
+    void display()
+    {
+        cout << this->data1 << endl
+             << this->data2;
+    }
+};
+
+int main()
+{
+    Myclass<int, float> obj(2, 7.8);
+    obj.display();
+
     return 0;
 }
