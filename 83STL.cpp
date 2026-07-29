9ea0bbf3aa9e7ee4b3de43c6e53256170e527ba2
anshikasisodia4 <anshikasisodiya81@gmail.com>
2026-07-29 07:07:19 +0530

    Initial commit

---
+++ 83STL.cpp
@@ -0,0 +1,18 @@
+
+ // stl saves time
+/*
+Components 
+1. Containers : object which stores data and use template classes
+2. Algorithm  : procedure to process data and uses template functions
+3. Iterators :  object which points to an element of a container and connect algorithms with containers
+
+/**
+ Containers:
+  1. Sequence Containers : stores data in liner fashion example : dequeue , vector ,list
+     vector: ( fast random access) ( slow insertion and deletion in middle) ( fast insertion at the end)
+     list: slow random access , (fast insertion at middle) , ( insertion / deletion fast at end)
+  2. Associative Containers : direct access example: map , multimap , set, multiset
+     all operations are fast except random access
+  3. Derived Containers : real world modelling example: stack , queue , priority queue
+     depends 
+ */
